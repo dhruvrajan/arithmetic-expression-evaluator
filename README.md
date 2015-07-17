d50c11e19a5452ac82d4dbae89f61efcd004cc41
Dhruv Rajan <dhruv@krishnaprem.com>
2015-07-17 07:21:38 -0700

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # arithmetic-expression-evaluator
 Expression Evaluator using Dijkstra's Two-Stack Algorithm
+
+## USAGE
+----
+
+Call method Parser.parse(s: str).
+
+Format of s: A valid arithmetic expression, with each operation (+, -, *, /) enclosed
+by parentheses.
+
+Example:
+Python```
+>>> s = "(1 + ((2 * (4/6)) + 3))"
+>>> Parser().parse(s)
+[out]: 5.3333
+```
--- test_parser.py
+++ test_parser.py
@@ -3,14 +3,17 @@ __author__ = 'dhruv'
 import unittest
 from parser import Parser
 from unittest import TestCase
-
+import operator
+operations = {"+": operator.add, "*": operator.mul, "-": operator.sub,
+              "/": operator.truediv}
 class TestParser(TestCase):
 
-    expression = "(1 + (2 + 3))"
+    # expression = "(((1 + (2 + (3 * (7 + 8))))/6) - 1)"
+    expression = "(1 + ((2 * (4/6)) + 3))"
 
     def test_parser(self):
         parser = Parser()
-        parser.parse(self.expression)
+        print(parser.parse(self.expression))
 
 if __name__ == "__main__":
     unittest.main()
